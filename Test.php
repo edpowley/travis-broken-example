6e87850b02a61087b64bdc252dea71dc91296035
edpowley <edward.powley@falmouth.ac.uk>
2017-06-16 14:22:13 +0100

    rsuigher

--- Test.php
+++ Test.php
@@ -5,6 +5,12 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+	public function testTwoPlusTwo() {
+		$this->assertEquals(2+2,4);
+  	}
+	public function testThreePlusThree() {
+		$this->assertEquals(3+3,5);
+  	}
 }
 
 ?>
